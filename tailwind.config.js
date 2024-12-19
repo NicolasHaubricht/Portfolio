428919c752d7cd09b28da50691c62556b77b0b60
Nicolas <haubrichtnicolas@gmail.com>
2024-12-19 12:23:37 -0300

    Corrigindo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -117,5 +118,5 @@ export default {
     		}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 }
