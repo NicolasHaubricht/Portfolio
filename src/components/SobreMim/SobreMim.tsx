77125e747b84db3902aaf3f7ec16796e7fc7de74
Nicolas <haubrichtnicolas@gmail.com>
2025-02-13 14:17:22 -0300

    Atualizando Sobre Mim

--- src/components/SobreMim/SobreMim.tsx
+++ src/components/SobreMim/SobreMim.tsx
@@ -29,13 +29,12 @@ const SobreMim = () => {
                                     <ul>CSS</ul>
                                     <ul>JavaScript</ul>
                                     <ul>React</ul>
-                                    <ul>TypeScript</ul>
                                     <ul>Git e GitHub</ul>
                                 </div>
                                 <div>
                                     <ul>Python</ul>
+                                    <ul>Flask</ul>
                                     <ul>SQL</ul>
-                                    <ul>AWS</ul>
                                     <ul>C++</ul>
                                 </div>
                             </AccordionContent>
